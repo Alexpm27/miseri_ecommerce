a80cc9fa1c2164e1ef7ebbad9ab36c9844968025
Alexpm27 <213523@ids.upchiapas.edu.mx>
2024-02-20 10:57:23 -0600

    first commit

---
+++ src/main/java/ampei/miseriecommerce/MiseriEcommerceApplication.java
@@ -0,0 +1,13 @@
+package ampei.miseriecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiseriEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiseriEcommerceApplication.class, args);
+    }
+
+}
